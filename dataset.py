c4fc49a70243e4fb440054265f128036a04800fd
Sehajbal01 <158218098+Sehajbal01@users.noreply.github.com>
2025-07-12 20:59:42 -0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,11 +3,11 @@ import os
 # This is where we’ll build the new dataset structure
 target_base = '/workspace/sehajdeepbal/create_dataset'
 
-# Make sure all the main folders exist — even if we’re only filling cars_train for now
+
 for split in ['cars_train', 'cars_val', 'cars_test']:
     os.makedirs(os.path.join(target_base, split), exist_ok=True)
 
-# Loop through just the dataset we care about (cars_train)
+# Looping through just the dataset we care about (cars_train)
 for subdataset in ['cars_train']:
     # Define where the source data is and where we’re writing new output
     source_base = os.path.join('/workspace/data/srncars', subdataset)
@@ -31,6 +31,6 @@ for subdataset in ['cars_train']:
             # Loop through all the files in the images folder
             for fname in os.listdir(img_dir):
                 if fname.endswith('.png'):
-                    # Instead of copying the real image, we just create an empty placeholder with the same name (Not sure if this is what we want)
+                    # Instead of copying the real image, we just create an empty placeholder with the same name (Not sure if this is what we want? lmk)
                     placeholder_path = os.path.join(shapenet_sar_path, fname)
                     open(placeholder_path, 'a').close()  # creates an empty .png file
