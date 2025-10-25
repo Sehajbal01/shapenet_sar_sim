f35dc4245038447cb2e12f00077d617bab8740c9
JhihYang Wu <jhihyang_wu@outlook.com>
2025-10-24 20:13:52 -0700

    update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 rm figures/tmp/*.png
+rm figures/tmp_ray_tracer/*.png
 
-CUDA_VISIBLE_DEVICES=7 python3.8 render_images.py
+#CUDA_VISIBLE_DEVICES=7 python3.8 render_images.py
+CUDA_VISIBLE_DEVICES=7 python render_images.py
 
-rm figures/tmp/*.png
+# rm figures/tmp/*.png
+# rm figures/tmp_ray_tracer/*.png
