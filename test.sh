d85772ba748aa87fe4ae486ae3e78d8ddedaac9a
alexberian <berian@email.arizona.edu>
2025-10-27 14:28:30 -0700

    found the ray tracing error

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 rm figures/tmp/*.png
 rm figures/tmp_ray_tracer/*.png
 
-#CUDA_VISIBLE_DEVICES=7 python3.8 render_images.py
-CUDA_VISIBLE_DEVICES=7 python render_images.py
+CUDA_VISIBLE_DEVICES=5 python3.8 render_images.py
+#CUDA_VISIBLE_DEVICES=7 python render_images.py
 
 # rm figures/tmp/*.png
 # rm figures/tmp_ray_tracer/*.png
