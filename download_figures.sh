80c283a7ec5c863bad370534ee02095d10b726cb
alexberian <berian@email.arizona.edu>
2026-07-13 07:43:43 -0700

    updated download figs to include paper

--- download_figures.sh
+++ download_figures.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 REMOTE_HOST="engr-mahala01s.engr.arizona.edu"
 REMOTE_USER="berian"
-REMOTE_PATH="/workspace/berian/shapenet_sar_sim/figures/"
-LOCAL_PATH="/home/berian/Documents/remote-figures/"
+REMOTE_BASE="/workspace/berian/shapenet_sar_sim"
 
-rm -rf "$LOCAL_PATH"/*
-mkdir -p "$LOCAL_PATH"
+# Each entry is "remote_subdir:local_path"
+FOLDERS=(
+    "figures:/home/berian/Documents/remote-figures"
+    "latex:/home/berian/Documents/paper"
+)
+
 RSYNC_ARGS=(-avz --progress)
 if [ -n "$1" ]; then
     RSYNC_ARGS+=(--include="*$1*" --exclude="*")
 fi
-rsync "${RSYNC_ARGS[@]}" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH" "$LOCAL_PATH"
+
+for entry in "${FOLDERS[@]}"; do
+    REMOTE_SUBDIR="${entry%%:*}"
+    LOCAL_PATH="${entry#*:}"
+    rm -rf "$LOCAL_PATH"/*
+    mkdir -p "$LOCAL_PATH"
+    rsync "${RSYNC_ARGS[@]}" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BASE/$REMOTE_SUBDIR/" "$LOCAL_PATH/"
+done
